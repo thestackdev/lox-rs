4cd3ecf61b11017944c08902e06ac9b251b5fd78
thestackdev <shanmukeshwar@icloud.com>
2026-09-14 12:21:23 +0530

    add ast definitions

---
+++ src/ast.rs
@@ -0,0 +1,55 @@
+#[derive(Debug, Clone)]
+pub enum Expr {
+    Number(f64),
+    StringLit(String),
+    Bool(bool),
+    Variable(String),
+    Binary {
+        left: Box<Expr>,
+        op: BinaryOp,
+        right: Box<Expr>,
+    },
+    Unary {
+        op: UnaryOp,
+        expr: Box<Expr>,
+    },
+}
+
+#[derive(Debug, Clone)]
+pub enum BinaryOp {
+    Add,
+    Sub,
+    Mul,
+    Div,
+    Equal,
+    NotEqual,
+    Less,
+    LessEqual,
+    Greater,
+    GreaterEqual,
+}
+
+#[derive(Debug, Clone)]
+pub enum UnaryOp {
+    Neagtive,
+    Not,
+}
+
+pub enum Stmt {
+    Let {
+        name: String,
+        initializer: Expr,
+    },
+    Print(Expr),
+    Block(Vec<Stmt>),
+    If {
+        condition: Expr,
+        then_branch: Box<Stmt>,
+        else_branch: Option<Box<Stmt>>,
+    },
+    While {
+        condition: Expr,
+        body: Box<Stmt>,
+    },
+    ExprStmt(Expr),
+}
